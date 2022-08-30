ab0ec9b919f41955f69418fe8222ecf06deebe73
taem1n1 <youman7953@naver.com>
2022-08-30 19:28:59 +0900

    kotlin 의존성 관리

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,15 @@ plugins {
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 	kotlin("plugin.jpa") version "1.6.21"
+	kotlin("kapt") version "1.4.10"
+
+	idea
 }
 
 group = "com.study"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
+val queryDslVersion = "5.0.0"
 
 repositories {
 	mavenCentral()
@@ -25,12 +29,24 @@ dependencies {
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	implementation("com.querydsl:querydsl-jpa:$queryDslVersion")
+	kapt("com.querydsl:querydsl-apt:$queryDslVersion:jpa")
+	kapt("org.springframework.boot:spring-boot-configuration-processor")
 }
 
 allOpen { // 추가적으로 열어줄 allOpen
 	annotation("javax.persistence.Entity")
 	annotation("javax.persistence.MappedSuperclass")
 	annotation("javax.persistence.Embeddable")
+}
+
+idea {
+	module {
+		val kaptMain = file("build/generated/source/kapt/main")
+		sourceDirs.add(kaptMain)
+		generatedSourceDirs.add(kaptMain)
+	}
 }
 
 tasks.withType<KotlinCompile> {
