aa9f4aff2b01d9b08bb60789bc02215285d720c6
taem1n1 <youman7953@naver.com>
2022-09-01 20:23:58 +0900

    Registration Lecture Controller

--- src/main/kotlin/com/study/kotlin/domain/lecture/controller/LectureController.kt
+++ src/main/kotlin/com/study/kotlin/domain/lecture/controller/LectureController.kt
@@ -2,10 +2,11 @@ package com.study.kotlin.domain.lecture.controller
 
 import com.study.kotlin.domain.lecture.dto.LectureRegistrationDto
 import com.study.kotlin.domain.lecture.service.LectureService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -17,7 +18,9 @@ class LectureController(
     @PostMapping("/registration/lecture")
     fun registrationLecture(
         @RequestBody lectureRegistrationDto: LectureRegistrationDto
-    ) {
+    ): ResponseEntity<LectureRegistrationDto> {
+
         lectureService.registrationLecture(lectureRegistrationDto)
+        return ResponseEntity.status(HttpStatus.CREATED).build()
     }
 }
